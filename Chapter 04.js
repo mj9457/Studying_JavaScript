30f6d8678b04bb7a38a6796bd376a515211ea1a1
mj9457 <mj9457@naver.com>
2021-10-06 18:30:29 +0900

    5th study day

--- Chapter 04.js
+++ Chapter 04.js
@@ -485,7 +485,7 @@
     // 기존 함수에 new 연산자를 붙여서 호출하면 해당 함수는 생성자 함수로 동작한다.
     // 함수명의 첫 문자를 댐누자로 쓰기를 권함 ( 생성자 함수 ex. new List )
 
-    // 생성자 함수가 동작하는 방식
+    // 첫번째 . 생성자 함수가 동작하는 방식
         // 1. 빈 객체 생성 및 this 바인딩
             // ★ 생성자 함수가 빈 객체를 생성하고 빈 객체는 자신을 생성자 함수의 prototype 프로퍼티를 가리키는 객체를 자신의 프로토 타입 객체로 설정 ★
         // 2. this를 통한 프로퍼티 생성
@@ -503,7 +503,345 @@
     // foo 객체 생성 
     var foo = new Person('foo'); // new 로 호출하면 Person은 생성자 함수로 동작
     console.log(foo.name);
+
+    // 두번째 . 객체 리터럴 방식과 생성자 함수를 통한 객체 생성 방식의 차이
+
+    // 객체 리터럴 방식으로 객체 생성
+    var foo = {
+        name : 'foo',
+        age : 30,
+        gender : 'male'
+    };
+    console.dir(foo);
+
+    //생성자 함수
+    function Person(name, age, gender, position) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    };
+
+    // Person 생성자 함수를 이용해 bar 객체 , baz 객체 생성
+    var bar = new Person('bar',33,'femal');
+    console.dir(bar);
+
+    var baz = new Person('baz',25,'male');
+    console.dir(bar);
+
+    // 차이는 프로토타입 객체(__proto__ 프로퍼티)에 있다.
+    // 차이나는 이유는 자바스크립트 생성 규칙 때문에
+    // 자바스크립트 객체는 자신을 생성한 생성자의 함수 prototype 프로퍼티가 가리키는 객체를 자신의 프로토타입 객체로 설정한다
+    // 객체 리터럴 방식 : 프로토타입 객체가 Object  ( 원본은 Ojbect.prototype)
+    // 생성자 함수 방식 : 프로토타입 객체가 Person ( 원본은 Person.prototype)
+
+    // 세번째 . new를 붙이지 않고 생성자 함수 호출 시의 오류
+    // 일반 함수 호출시 this가 window 전역 객체에 바인딩
+    // 생성자 함수 호출시 this가 새로 생성되는 빈 객체에 바인딩
+
+    //생성자 함수
+    function Person(name, age, gender, position) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    };
+    var qux = Person('qux', 20, 'man');
+    console.log(qux);
+
+    console.log(window.name);
+    console.log(window.age);
+    console.log(window.gender);
+
+    // 생성자 함수의 구분은 일반적으로 첫글자를 대문자로 표기하는 네이밍 규칙, new 사용
+
+    // 강제로 인스턴스 생성하기
+    function A(arg) {
+        if (!(this instanceof arguments.callee)) // 매우 중요 표현식 / 함수가 작성자의 의도와 다르게 호출되어도 문제 발생 X, new로 반한된 인스턴스를 사용할 것을 확신하고 자신의 코드 작성 가능
+        return new A(arg);
+        this.value = arg ? arg : 0;
+    }
+
+    var a = new A(100);
+    var b = A(10);
+
+    console.log(a.value);
+    console.dir(a);
+    console.log(b.value);
+    console.dir(b);
+    console.log(globalThis.value);
 */
 
+/* 4.4.2.4 call과 apply 메서드를 이용한 명시적인 this 바인딩
+    //this를 특정 객체에 명시적으로 바인딩 하는 방법
+    // 4.2.3 함수 객체의 기본 프로퍼티인 apply() 와 call() 메서드
+    // 두 메서드는 모든 함수의 부모 객체인 Function.prototype 객체의 메서드이다
+    // 모든 함수는 다음과 같은 형태로 apply() 메서드를 호출하는 것이 가능하다
 
+    // apply() 메서드 호출 방법
+    function.apply(thisArg, argArray)
+
+    // thisArg는 apply() 메서드를 호출한 함수 내부에서 사용한 this에 바인딩할 객체를 가리킴
+    // argArray는 함수 호출할때 넘길 인자들의 배열을 가리킴
+
+    // apply() 메서드 호출 주체 = 함수
+    // apply() 메서드도 this를 특정 객체에 바인딩 할뿐
+    // 본질적인 기능은 "함수 호출"이다.
+
+    // 정리 : argArray 배열을 자신을 호출한 함수의 인자로 사용하되,
+    // 이 함수 내부에서 사용된 this는 thisArg 객체로 바인딩해서 함수를 호출하는 기능
+    
+    // apply() 메서드를 이용한 명시적인 this 바인딩
+    //생성자 함수
+    function Person(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    // foo 빈 객체 생성
+    var foo = {};
+
+    // apply() 메서드 호출
+    Person.apply(foo, ['foo',30,'male']); // apply()메서드는 배열을 넘김
+    console.dir(foo);
+    console.log(foo);
+
+    // call() 메서드 호출
+    Person.call(foo, 'foo',30,'male'); // call() 메서드는 배열의 인자를 각각 한개씩 넘김
+    console.dir(foo);
+    console.log(foo);
+
+    // apply()나 call() 메서드의 장점 : this를 원하는 값으로 명시적으로 매핑해서 특정 함수나 메서드를 호출할 수 있다.
+
+    // apply()메서드를 활용한 arguments 객체의 배열 표준 메서드 slice() 활용 코드    
+    function myFunction() {
+        console.dir(arguments); // __proto__ = Object
+
+        //arguments.shift(); // arguments 배열은 실제 배열이 아닌 유사 배열 객체로 pop. shift 등 표준 배열 메서드를 사용 할 수 없다.
+
+    var args = Array.prototype.slice.apply(arguments); // 배열 메서드가 있는것 처럼 처리하는 코드
+    console.dir(args); // __proto__ = Array[0]
+    }
+
+    myFunction(1,2,3);
+
+    // slice() 활용법 코드
+    var arrA = [1,2,3];
+    var arrB = arrA.slice(0); // 아무 인자 안넘기면 전체 배열 복사
+    var arrC = arrA.slice(); // 아무 인자 안넘기면 전체 배열 복사
+    var arrD = arrA.slice(1); // END 인자 넘기지 않으면 기본값은 배열의 lenght값 즉 slcie(1,3);
+    var arrF = arrA.slice(1,3); // arrD와 동일하다
+    var arrE = arrA.slice(1,2); 
+
+    console.log(arrA);
+    console.log(arrB);
+    console.log(arrC);
+    console.log(arrD);
+    console.log(arrE);
+    console.log(arrF);
+*/
+
+/* 4.4.3.1 함수 리턴 규칙 1) 일반 함수나 메서드는 리턴값을 지정하지 않을경우, undefined 값이 리턴된다.
+    // 자바스크립트 함수는 항상 리턴값을 반환한다.
+
+    // return 문 없는 일반 함수의 리턴값 확인
+
+    // noReturnFunc() 함수
+    var noReturnFunc = function() {
+        console.log('This function has no return statement.');
+    };
+
+    var result = noReturnFunc();
+    console.log(result);
+
+*/
+
+/* 4.4.3.2 함수 리턴 규칙 2) 생성자 함수에서 리턴값을 지정하지 않을 경우 생성된 객체가 리턴된다.
+    // 생성자 함수가에서 명시적으로 객체를 리턴했을 경우
+
+    //Person() 생성자 함수
+    function Person(name, age, gender) {
+        this.na으로me = name;
+        this.age = age;
+        this.gender = gender;
+
+        // 명시적 다른 객체 반환
+        return {name:'bar', age:20, gender:'woman'};
+    }
+    var foo = new Person('foo',30,'male');
+    console.log(foo);
+    console.dir(foo);
+
+    // 생성자 함수의 리턴값으로 넘긴값이 객체가 아닌 불린, 숫자, 문자열의 경우는 이러한 리턴값을 무시하고 this로 바인딩된 객체가 리턴된다.
+    function Person(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+
+        return 100; // 불린, 숫자, 문자열의 경우는 이러한 리턴값을 무시한다.
+    }
+
+    var foo = new Person('foo',30,'male'); // 위에 리턴값은 무시하고 해당 객체가 바인딩 된다.
+    console.dir(foo);
+    console.log(foo);
+*/
+
+/* 4.5.1 프로토타입 체이닝
+    // 프로토타입 체이닝 두가지 의미 :  js에서 특정 객체의 프로퍼티나 메서드에 접근시 , 그 p 나 m 이 없다면 [[Prototype]] 링크를 따라 프로토타입 객체의 p를 차례대로 검색하는 것
+
+    // 모든 객체는 자신의 부모인 프로토타입 객체를 가리키는 참조링크 형태의 숨겨진 프로퍼티 : 암묵적 프로퍼티 링크(implicit prototype link)
+    // [[Prototype]] 프로퍼티에 저장된다
+
+    // prototype 프로퍼티가 가리키는 프로토타입 객체를 자신의 부모 객체로 설정하는 [[Prototype]]링크로 연결한다.
+
+    // prototype 프로퍼티와 [[Prototype]] 링크 구분하는 방법
+    // 차이점 : js의 객체 생성 규칙을 알아야 한다.
+    // prototype 프로퍼티 : 함수의 입장에서 자신과 링크된 프로토타입 객체를 가리킴
+    // [[Prototype]] 링크 : 객체의 입장에서 자신의 부모 객체인 프로토타입 객체를 내부의 숨겨진 링크로 가리킴
+    
+    function Person(name) {
+        this.name = name;
+    }
+
+    var foo = new Person('foo');
+
+    console.dir(Person);
+    console.dir(foo);
+
+    // 생성된 객체의 실제 부모 역할은 생성자 자신이 아닌 생성자의 prototype 프로퍼티가 가리키는 프로토타입 객체이다.
+*/
+
+/* 4.5.2 객체 리터럴 방식으로 생성된 객체의 프로토타입 체이닝
+    var myObject = {
+        name : 'foo',
+        sayName : function() {
+            console.log('My name is ' + this.name);
+        }
+    };
+
+    myObject.sayName();
+    console.log(myObject.hasOwnProperty('name')); // hasOwnProperty() 메서드는 이 메서드를 호출한 객체에 인자로 넘긴 문자열 이름의 프로퍼티나 메서드가 있는지 체크하는 js 표준 api 함수다.
+    console.log(myObject.hasOwnProperty('nickName'));
+    myObject.sayNickName();
+*/
+
+/* 4.5.3 생성자 함수로 생성된 객체의 프로토타입 체이닝
+    function Person(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    var foo = new Person('foo',30,'male'); // 이 객체의 프로토타입 객체는 Person.prototype 이다.
+    console.log(foo.hasOwnProperty('name'));
+
+    console.dir(Person.prototype);
+*/
+
+/* 4.5.4 프로토타입 체이닝의 종점
+    // js에서 Object.prototype 객체는 프로토타입 체이닝의 종점이다.
+    // ★객체 리터럴, 생성자 함수 방식에 상관없이 js의 모든 객체는 프로토타입 체이닝으로 Object.prototype 객체가 가진 프로퍼티와 메서드에 접근하고, 서로 공유 가능하다. ★
+    
+*/
+
+/* 4.5.5 기본 데이터 타입 확장
+    // Object.prototype에 정의한 메서드는 모든 객체의 표준 메서드라고 볼 수 있다.
+        // Number.prototype = 숫자 표준 메서드 / String.prototype = 문자열 표준 메서드 / Array.prototype = 배열 표준 메서드 로 정의
+        // 위 객체들도 Object.prototype을 자신의 프로토타입으로 가지고 있어서 프로토타입 체이닝으로 연결된다.
+        // 프로토타입 객체에도 사용자가 직접 정의한 메서드 추가 허용한다.
+
+    // String 기본 타입에 메서드 추가
+    String.prototype.testMethod = function() {
+        console.log('This is the String.prototype.testMethod()');
+    };
+
+    var str = "this is test";
+    str.testMethod();
+
+    console.dir(String.prototype);
+*/
+
+/* 4.5.6 프로토타입도 js 객체다.
+    // 함수 생성시, 자신의 p 프로퍼티에 연결되는 p 객체는 default 로 constructor 프로퍼티만 가진 객체다
+    // 프로토타입 객체 = js 객체
+    // 동적으로 프로퍼티 추가 / 삭제 가능
+    // 하게되면 실시간으로 프로토타입 체이닝에 실시간 반영
+    
+    function Person(name) {
+        this.name = name;
+    }
+
+    var foo = new Person('foo');
+
+    Person.prototype.sayHello = function () {
+        console.log('Hello');
+    }
+
+    foo.sayHello();
+*/
+
+/* 4.5.7 프로토타입 메서드와 this 바인딩
+    // this의 바인딩은 메서드 호출 패턴에서의 this 는 그 메서드를 호출한 객체에 바인딩 된다.
+
+    function Person(name) {
+        this.name = name;
+    }
+
+    // getName() 프로토타입 메서드
+    Person.prototype.getName = function () {
+        return this.name;
+    }
+
+    var foo = new Person('foo');
+
+    console.log(foo.getName());
+
+    // Person.prototype 객체에 name 프로퍼티 동적 추가
+    Person.prototype.name = 'person';
+
+    console.log(Person.prototype.getName());
+*/
+
+/* 4.5.8 디폴트 프로토타입은 다른 객체로 변경이 가능하다
+    // 변경할때 생성자 함수의 p 객체가 변경되면 , 변경된 시점 이후에 생성된 객체는 변경된 p 객체로 [[Prototype]] 링크를 연결한다는 점
+
+    function Person(name) {
+        this.name = name;
+    }
+    console.log(Person.prototype.constructor);
+
+    var foo = new Person('foo');
+    console.log(foo.country);
+
+    Person.prototype = {
+        country : 'Korea',
+    };
+    console.log(Person.prototype.constructor);
+
+    var bar = new Person('bar');
+    console.log(foo.country);
+    console.log(bar.country);
+    console.log(foo.constructor);
+    console.log(bar.constructor);
+*/
+
+/* 4.5.9 객체의 프로퍼티 읽기나 메서드를 실행할 때만 프로토타입 체이닝이 동작한다
+    // 객체의 특정 프로퍼티를 읽으려고 할 때, 프로퍼티가 해당 객체에 없는 경우 프로토타입 체이닝이 동작한다
+    // 반대로 객체에 존재하면 프로토타입 체이닝은 동작하지 않는다
+    // 객체에 없는 프로퍼티에 값을 쓰려고 할때 동적으로 객체에 프로퍼티를 추가하기 때문이다.
+
+    function Person(name) {
+        this.name = name;
+    }
+
+    Person.prototype.country = 'Korea';
+
+    var foo = new Person('foo');
+    var bar = new Person('bar');
+    console.log(foo.country);
+    console.log(bar.country);
+    foo.country = 'USA'; // 프로토타입 체이닝이 동작 한게 아님 > 프로퍼티값이 동적으로 생성된 것
+
+    console.log(foo.country);
+    console.log(bar.country);
+*/
 
